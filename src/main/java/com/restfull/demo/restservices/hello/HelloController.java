d9519b2f147e2e9cb08dbd6ca2045997128d48f8
GF <GF@LAPTOP-UM1C21PJ>
2023-08-01 17:01:11 +0700

    update

--- src/main/java/com/restfull/demo/restservices/hello/HelloController.java
+++ src/main/java/com/restfull/demo/restservices/hello/HelloController.java
@@ -1,5 +1,9 @@
 package com.restfull.demo.restservices.hello;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	private MessageSource messageSource;
+	
+	public HelloController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@RequestMapping(method = RequestMethod.GET, path = "/hello")
 	public String hello() {
@@ -21,5 +31,12 @@ public class HelloController {
 	@GetMapping(path = "/helloBean")
 	public HelloBean helloBean() {
 		return new HelloBean("Hello Bean!!");
+	}
+	
+	@GetMapping(path = "/helloInternationalization")
+	public String helloInternationalization() {
+		Locale locale =LocaleContextHolder.getLocale();
+		return messageSource.getMessage("msg.greating.goodmorning", null, "Defaulte Message", locale);
+		
 	}
 }
